9fd46306462ba854a5bffe3d765fb42dd2559bf8
limyoursun <limyoursun@gmail.com>
2024-02-15 17:30:54 +0900

    counter 적용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Button from "./Button"
 import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter(prev => prev + 1)
+  console.log('redered');
   return (
     <div>
-      <h1 className={styles.title}>welcome back! to react</h1>
-      <Button text={'하이연'} />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ import "./Button.module.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  //<React.StrictMode>
     <App/>
-  </React.StrictMode>
+  //</React.StrictMode>
+  /*
+  React.StrictMode 테그에 감싸져 있어서 그렇다고 합니다.
+  StrictMode는 create-react-app로 설치했을 때 기본적으로 생성되는 테그로,
+  해당 테그로 감싸져 있는 경우 자손까지 검사한다해서 두 번 실행된다고 합니다.
+  */
 );
