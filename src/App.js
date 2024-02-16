6e45a86cff5a65319686ebb9a741bb5626c5dc16
unknown <limyoursun@gmail.com>
2024-02-17 01:23:50 +0900

    41percent

--- src/App.js
+++ src/App.js
@@ -3,29 +3,19 @@ import styles from "./App.module.css";
 import { useState , useEffect} from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("")
-  const onClick = () => setCounter(prev => prev + 1)
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => {
-    console.log('i run only once!')
-  }, []) // 모든 상황에서 함수 실행함
-  useEffect(() => {
-      console.log("i run when 'keyword' changes.");
-  }, [keyword]) // 해당 변수가 변화될 때만 함수를 실행함
-  useEffect(() => {
-    console.log("i run when 'counter' changes.");
-}, [counter])
-  return (
+  const [value, setValue] = useState(false);
+  const onClick = () => {
+    setValue(prev => !prev)
+  }
+  function Hello(){
+    return <h1>hi!</h1>
+  }
+  return(
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text" placeholder="Search here.."/>
-      <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {value ? <Hello/> : null}
+      <button onClick={onClick}>{value ? 'show' : 'hide'}</button>
     </div>
-  );
+  )
 }
 
 export default App;
