f21957e0fb43042fbe12fba9f450f3de30150ee6
unknown <limyoursun@gmail.com>
2024-02-17 16:59:11 +0900

    43percent

--- src/App.js
+++ src/App.js
@@ -3,19 +3,33 @@ import styles from "./App.module.css";
 import { useState , useEffect} from "react";
 
 function App() {
-  const [value, setValue] = useState(false);
-  const onClick = () => {
-    setValue(prev => !prev)
-  }
+  const [value, setValue] = useState(false)
+  const onClick = () => setValue(prev => !prev)
+  // function Hello(){
+  //   useEffect(() => {
+  //     console.log("created! :)");
+  //     return() => {console.log("destroyed :(")} // cleanUp function
+  //   })
+  //   return <h1>안녕하시와요</h1>;
+  // }
+  
   function Hello(){
-    return <h1>hi!</h1>
+    function byeFn(){
+      console.log("destroyed :(") // cleanUp function
+    }
+    function hiFn(){
+      console.log("created! :)");
+      return byeFn;
+    }
+    useEffect(hiFn, [])
+    return <h1>안녕하시와요</h1>;
   }
-  return(
-    <div>
-      {value ? <Hello/> : null}
-      <button onClick={onClick}>{value ? 'show' : 'hide'}</button>
-    </div>
-  )
+return(
+  <div>
+    {value ? <Hello/> : null }
+    <button onClick={onClick}>{value ? "show" : "hide"}</button>
+  </div>
+)
 }
 
 export default App;
